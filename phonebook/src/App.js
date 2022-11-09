5a5671ad418d572bfa6cee1866cebd466cd36202
Alexander Manns <dante690dark@hotmail.com>
2022-11-08 21:34:58 -0500

    remove de hardcode from the state

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -6,12 +6,7 @@ import "./style.css";
 
 const App = () => {
   const [state, setState] = useState(false);
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
   const [filterNames, setFilterNames] = useState(persons);
