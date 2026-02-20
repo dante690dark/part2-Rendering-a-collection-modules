d6341cd8e58fc61aca96661753e8cc23c3560410
Alexander <alexander@Alexanders-MacBook-Pro.local>
2026-02-19 22:50:05 -0500

    fix duplicate validation for name and add notification when update number

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -18,33 +18,46 @@ const App = () => {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    getPersons().then((response) => {
-      setPersons([...response.data]);
+    getPersons().then(({ data }) => {
+      setPersons([...data]);
     });
   }, []);
 
+  useEffect(() => {
+    if (!message) return;
+
+    const timeout = setTimeout(() => {
+      setMessage(null);
+    }, 5000);
+
+    return () => clearInterval(timeout);
+  }, [message]);
+
   const addName = (event) => {
     event.preventDefault();
-    const findPerson = persons.find((person) => person.name === data.name);
+    const findPerson = persons.find(
+      (person) => person.name.toLowerCase() === data.name.toLowerCase(),
+    );
     const formattedName = data.name.at(0).toUpperCase() + data.name.slice(1);
 
     if (findPerson) {
       if (
         window.confirm(
-          `${data.name} is already added to phonebook, replace the old number with a new one`,
+          `${formattedName} is already added to phonebook, replace the old number with a new one`,
         )
       ) {
         updatePerson(findPerson.id, {
           ...findPerson,
           number: data.number,
         })
-          .then(({ data }) =>
+          .then(({ data }) => {
+            setMessage(`Update ${data.name}`);
             setPersons((prevState) =>
               prevState.map((person) =>
                 person.id === data.id ? data : person,
               ),
-            ),
-          )
+            );
+          })
           .catch((error) => console.error(error));
         setData({ name: "", number: "" });
         return;
@@ -54,17 +67,17 @@ const App = () => {
       return;
     }
 
-    createPerson({ ...data, name: formattedName }).then((reponse) => {
-      setMessage("Add people");
+    createPerson({ ...data, name: formattedName }).then(({ data }) => {
+      setMessage(`Add ${data.name}`);
       setTimeout(() => {
         setMessage(null);
       }, 5000);
       setPersons((prevState) => [
         ...prevState,
         {
-          name: reponse.data.name,
-          number: reponse.data.number,
-          id: reponse.data.id,
+          name: data.name,
+          number: data.number,
+          id: data.id,
         },
       ]);
     });
