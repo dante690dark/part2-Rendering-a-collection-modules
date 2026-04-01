ce45a884c368310db46ea3654207b3ba1f62dc86
Alexander <alexander@Alexanders-MacBook-Pro.local>
2026-04-01 12:21:23 -0500

    add some refactors

--- countries/services/country.js
+++ countries/services/country.js
@@ -1,7 +1,12 @@
 import axios from "axios";
-const url = "https://studies.cs.helsinki.fi/restcountries/api";
+const URL_COUNTRY = "https://studies.cs.helsinki.fi/restcountries/api";
+const URL_WEATHER = "https://api.openweathermap.org/data/3.0/onecall?";
 
-const getAllCountries = () => axios.get(`${url}/all`);
-const getCountry = (data) => axios.get(`${url}/name/${data}`);
+const getAllCountries = () => axios.get(`${URL_COUNTRY}/all`);
+const getCountry = (data) => axios.get(`${URL_COUNTRY}/name/${data}`);
+const sendCountry = (latitude, longitude, api_key) =>
+  axios.get(
+    `${URL_WEATHER}lat=${latitude}&lon=${longitude}&exclude=hourly,daily&appid=${api_key}`,
+  );
 
-export { getAllCountries, getCountry };
+export { getAllCountries, getCountry, sendCountry };
--- countries/src/components/Country.jsx
+++ countries/src/components/Country.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { sendCountry } from "../../services/country";
 
 const Country = ({ countryDetail }) => {
+  const api_key = import.meta.env.VITE_SOME_KEY;
+  console.log(api_key);
+
+  // const [prueba, setPrueba] = useState({});
+
   const {
     name: { common },
     capital,
     area,
     languages,
     flags: { png, alt },
+    latlng: [latitude, longitude],
   } = countryDetail;
   const values = Object.values(languages);
+
+  useEffect(() => {
+    sendCountry(latitude, longitude, api_key)
+      .then(({ data }) => {
+        console.log("si funciona", data);
+      })
+      .catch((err) => console.log("no sirve", err));
+  }, [latitude, longitude, api_key]);
+
   return (
     <>
       <h1>{common}</h1>
